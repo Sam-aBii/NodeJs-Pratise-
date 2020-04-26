672d50fa9433f74d042eb578ebe328c661cb44b9
Rana Abid <62594563+Sam-aBii@users.noreply.github.com>
2020-04-27 00:02:31 +0500

    Update app.js

--- axiom-class-2/server-side-app/src/app.js
+++ axiom-class-2/server-side-app/src/app.js
@@ -12,25 +12,25 @@ console.log(newPathToPublicFolder);
 
 
 
-// console.log(__dirname);
-// console.log(__filename)
+console.log(__dirname);
+console.log(__filename)
 
 
-// app.get("/" , (req,res) => {
-//     res.send("<h1>Wellcome to the express world</h1>")
-// })
+app.get("/" , (req,res) => {
+    res.send("<h1>Wellcome to the express world</h1>")
+})
 
-// app.get("/helloworld" , (req,res) => {
-//     res.send({
-//         name: "abid",
-//         age: 15
-//     })
-// })
+app.get("/helloworld" , (req,res) => {
+    res.send({
+        name: "abid",
+        age: 15
+    })
+})
 
-// app.get("/about" , (req,res) => {
-//     res.send("Wellcome to the about page")
-// })
+app.get("/about" , (req,res) => {
+    res.send("Wellcome to the about page")
+})
 
-// app.listen(3000, () => {
-//     console.log(" server is up and listening to the port 3000")
-// })
+app.listen(3000, () => {
+    console.log(" server is up and listening to the port 3000")
+})
